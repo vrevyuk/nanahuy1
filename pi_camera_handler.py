e0f95902ff341dca19da01638da6e312efde52dd
Vitalii Reviuk <vrevuyk@gmail.com>
2023-12-10 19:41:37 +0200

    ..

--- pi_camera_handler.py
+++ pi_camera_handler.py
@@ -19,8 +19,8 @@ class PiCameraHandler:
     def getFrames(self):
         while True:
             frame = self.picam2.capture_array()
-            print("frame_picam", frame)
-            frame = cv2.resize(frame, (0, 0), fx=0.3, fy=0.3)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            # frame = cv2.resize(frame, (0, 0), fx=0.3, fy=0.3)
 
             _, buffer = cv2.imencode(".png", frame)
             encodedBuffer = base64.b64encode(buffer)
