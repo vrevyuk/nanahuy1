bb4211c01995b03892155bb65f1759eda7a830cb
Vitalii Reviuk <vrevyuk@gmail.com>
2023-12-10 17:32:45 +0000

    picam update

--- pi_camera_handler.py
+++ pi_camera_handler.py
@@ -1,7 +1,6 @@
 import sys
 import base64
 from picamera2 import Picamera2, Preview
-from libcamera import controls
 import time
 import cv2
 
@@ -10,8 +9,7 @@ class PiCameraHandler:
         self.picam2 = Picamera2()
         camera_config = self.picam2.create_preview_configuration()
         self.picam2.configure(camera_config)
-        self.picam2.start_preview(Preview.DRM)
-        self.picam2.set_controls({"AfMode": controls.AfModeEnum.Manual, "LensPosition": 0.0})
+        # self.picam2.start_preview(Preview.DRM)
         self.picam2.start()
         time.sleep(2)
         pass
@@ -22,6 +20,7 @@ class PiCameraHandler:
     def getFrames(self):
         while True:
             frame = self.picam2.capture_buffer()
+            print("frame_picam", frame)
             frame = cv2.resize(frame, (0, 0), fx=0.3, fy=0.3)
 
             _, buffer = cv2.imencode(".png", frame)
