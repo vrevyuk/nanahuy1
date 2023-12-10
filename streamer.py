0c4c367d253ec38c18572b3b8cb89b9155c944d9
Vitalii Reviuk <vrevuyk@gmail.com>
2023-12-10 13:16:26 +0200

    use 0.0.0.0 as ip

--- streamer.py
+++ streamer.py
@@ -6,16 +6,16 @@ from camera_handler import CameraHandler
 async def handle_socket_connection(websocket, path):
     print("connection has been opened")
     for frame in camera.getFrames():
-        print("got frame")
         await websocket.send(frame)
 
 try:
     camera = CameraHandler()
     print("Start websocket server ...")
-    socket_server = websockets.serve(handle_socket_connection, "localhost", 8001)
+    socket_server = websockets.serve(handle_socket_connection, "0.0.0.0", 8001)
     asyncio.get_event_loop().run_until_complete(socket_server)
     asyncio.get_event_loop().run_forever()
 except:
+    print("\r\n")
     camera.release()
 
 
