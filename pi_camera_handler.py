1cfc2bf8caeaaa7f496aa951b14d661f46961d24
Vitalii Reviuk <vrevyuk@gmail.com>
2023-12-11 18:33:53 +0000

    111

--- pi_camera_handler.py
+++ pi_camera_handler.py
@@ -1,16 +1,24 @@
 import sys
 import base64
 from picamera2 import Picamera2, Preview
+from picamera2.encoders import H264Encoder
+from libcamera import ColorSpace
 import time
 import cv2
 
 class PiCameraHandler:
     def __init__(self):
-        self.picam2 = Picamera2()
-        camera_config = self.picam2.create_preview_configuration()
-        self.picam2.configure(camera_config)
-        self.picam2.start()
+        picam2 = Picamera2()
+        for mode in picam2.sensor_modes:
+            print("MODE:", mode)
+
+        camera_config = picam2.create_still_configuration(main={"format": "XRGB8888"}, buffer_count=2)
+        #camera_config = picam2.create_still_configuration(lores={"size": (640, 480)})
+        picam2.configure(camera_config)
+        picam2.start()
         time.sleep(2)
+        self.picam2 = picam2
+        print("camera has been intialized")
         pass
 
     def release(self):
@@ -20,7 +28,8 @@ class PiCameraHandler:
         while True:
             frame = self.picam2.capture_array()
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            # frame = cv2.resize(frame, (0, 0), fx=0.3, fy=0.3)
+            # print(frame.shape) 1944, 2592
+            frame = cv2.resize(frame, (864, 648))
 
             _, buffer = cv2.imencode(".png", frame)
             encodedBuffer = base64.b64encode(buffer)
